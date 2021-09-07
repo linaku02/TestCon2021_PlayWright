ba15864c9fc1b1f9a9488b99c95e17b6d1292234
linakuch <lina.kuchalskyte@iterato.lt>
2021-09-07 11:40:20 +0300

    first test
    first test

--- tests/DuckSmokeTest.js
+++ tests/DuckSmokeTest.js
@@ -5,3 +5,9 @@ test('basic test', async ({ page }) => {
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
 });
+
+test('Visible logo test', async ({ page }) => {
+  await page.goto('https://duckduckgo.com/');
+  const logo = page.locator('#logo_homepage_link');
+  await expect(logo).toBeVisible();
+});
