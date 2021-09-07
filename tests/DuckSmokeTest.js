fee8b34a5e6581bf323a62bf6e775cd8175269a9
linakuch <lina.kuchalskyte@iterato.lt>
2021-09-07 11:51:07 +0300

    Update DuckSmokeTest.js
    clean up

--- tests/DuckSmokeTest.js
+++ tests/DuckSmokeTest.js
@@ -1,11 +1,5 @@
 const { test, expect } = require('@playwright/test');
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
-});
-
 test('Visible logo test', async ({ page }) => {
   await page.goto('https://duckduckgo.com/');
   const logo = page.locator('#logo_homepage_link');
