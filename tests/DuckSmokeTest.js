9b0b3fb7267104dfce23a9017d75126f91f84397
linakuch <lina.kuchalskyte@iterato.lt>
2021-09-07 13:03:02 +0300

    Update DuckSmokeTest.js
    Cr comments

--- tests/DuckSmokeTest.js
+++ tests/DuckSmokeTest.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('Visible logo test', async ({ page }) => {
+test('Checks that duckduckGo page can be opened', async ({ page }) => {
   await page.goto('https://duckduckgo.com/');
   const logo = page.locator('#logo_homepage_link');
   await expect(logo).toBeVisible();
